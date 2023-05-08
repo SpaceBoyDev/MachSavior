f17e440626342b2afc73be796faf8b337e8b4a6b
SpaceBoyDev <dario.mart.lop.03@gmail.com>
2023-05-08 02:33:13 +0200

    Script arquitecture reworked
    -PlayerManager and PlayerMovement arquitecture reworked
    -PlayerTest scene reworked
    -Wallrun detection
    -Wallrunning
    -Still no jump when wallrunning
    -PlayerConfig scriptable refined with Tooltips, Headers and Regions

--- MachSavior/Assets/_Data/Scripts/Player/PlayerManager.cs
+++ MachSavior/Assets/_Data/Scripts/Player/PlayerManager.cs
@@ -11,21 +11,9 @@ public class PlayerManager : MonoBehaviour
     [SerializeField]
     private GameObject root;
     [SerializeField]
-    [Tooltip("Points of origin for the ground raycast")]
-    private Transform[] raycastOrigin;
-    [SerializeField]
     private Camera playerCamera;
     [SerializeField]
     private Transform cameraPos;
-
-    [Header("Ground/Slopes")]
-    [SerializeField]
-    public float maxSlopeAngle = 40f;
-    [SerializeField]
-    private LayerMask groundMask;
-    private RaycastHit slopeHit;
-    [SerializeField]
-    int raycastHits = 0;
 
     private void Awake()
     {
@@ -37,40 +25,6 @@ public class PlayerManager : MonoBehaviour
         {
             Destroy(this);
         }
-    }
-
-    public bool checkFloor()
-    {
-        raycastHits = 0;
-        Debug.DrawLine(root.transform.position, new Vector3(root.transform.position.x, root.transform.position.y - 0.3f, root.transform.position.z), Color.red);
-
-        //Slope raycast
-        if (Physics.Raycast(root.transform.position + new Vector3(0, 0.1f, 0), Vector3.down, out slopeHit, 0.6f, groundMask, QueryTriggerInteraction.Ignore))
-        {
-            OnSlope();
-        }
-
-        //CheckGround raycast
-        for (int i = 0; i < raycastOrigin.Length; i++)
-        {
-            if (Physics.Raycast(raycastOrigin[i].transform.position + new Vector3(0, 0.1f, 0), Vector3.down, 0.25f, groundMask, QueryTriggerInteraction.Ignore))
-                raycastHits++;
-
-            Debug.DrawLine(raycastOrigin[i].transform.position, new Vector3(raycastOrigin[i].transform.position.x, raycastOrigin[i].transform.position.y - 0.1f, raycastOrigin[i].transform.position.z));
-        }
-
-        return raycastHits >= 1;
-    }
-
-    public bool OnSlope()
-    {
-        float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
-        return angle < maxSlopeAngle && angle != 0;
-    }
-
-    public Vector3 SlopeDirection(Vector3 moveDirection)
-    {
-        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
     }
 
     public GameObject GetRoot()
@@ -86,10 +40,5 @@ public class PlayerManager : MonoBehaviour
     public Transform GetCameraPos()
     {
         return cameraPos;
-    }
-
-    public RaycastHit GetSlopeHit()
-    {
-        return slopeHit;
     }
 }
