1d1e133d394abbe0fc52454f50e89fadc8844b9d
SpaceBoy <dario.mart.lop.03@gmail.com>
2023-07-07 18:35:50 +0200

    Turbina hechitaaa
    Turbina guachi, faltan un par de ajustes pero está más que utilizable

--- MachSavior/Assets/_Data/Scripts/TimeSystem/Props/Turbine.cs
+++ MachSavior/Assets/_Data/Scripts/TimeSystem/Props/Turbine.cs
@@ -1,13 +1,17 @@
 using System;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.Serialization;
+
 [RequireComponent(typeof(AnimatedTimeObject))]
 public class Turbine : MonoBehaviour
 {
     [SerializeField] private float speed = 0.2f;
     [SerializeField] private float airForce = 0.2f;
     [SerializeField] private GameObject turbine;
-    [SerializeField] private Collider triggerCollider;
+    [SerializeField] private GameObject airCollider;
+    [SerializeField] private ParticleSystem airParticles;
+    [SerializeField] private bool isActive;
 
     private AnimatedTimeObject _animatedTimeObject;
 
@@ -15,10 +19,59 @@ public class Turbine : MonoBehaviour
     {
         _animatedTimeObject = GetComponent<AnimatedTimeObject>();
         RotateTurbine();
-        if(_animatedTimeObject.getIsStopped)
-            _animatedTimeObject.StopTime();
-        else 
-            _animatedTimeObject.ResumeTime();
+    }
+
+    private void Update()
+    {
+        //if (_animatedTimeObject.getIsStopped)
+        //{
+        //        ChangeIsStopped(isActive);
+        //    
+        //}
+        //else
+        //{
+        //        _animatedTimeObject.ResumeTime();
+        //        airParticles.Play();
+        //        speed = 3f;
+        //}
+        
+        ChangeIsStopped(_animatedTimeObject.IsActive());
+    }
+
+    private void ChangeIsStopped(bool state)
+    {
+        if (state == isActive)
+        {
+            return;
+        }
+        else if (state == false && isActive == true)
+        {
+            OnExitActive();
+        }
+        else if (state == true && isActive == false)
+        {
+            OnEnterActive();
+        }
+    
+        isActive = state;
+    }
+
+    private void OnEnterActive()
+    {
+        print("Entrada");
+        _animatedTimeObject.ResumeTime();
+        airParticles.Play();
+        speed = 3f;
+        airCollider.SetActive(true);
+    }
+
+    private void OnExitActive()
+    {
+        print("Salida");
+        _animatedTimeObject.StopTime();
+        airParticles.Stop();
+        speed = 0.2f;
+        airCollider.SetActive(false);
     }
     
     /// <summary>
@@ -26,8 +79,10 @@ public class Turbine : MonoBehaviour
     /// </summary>
     private void RotateTurbine()
     {
-        _animatedTimeObject.tweenAnim = transform.DORotate(new Vector3(0f, 0f, 180f), speed, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Incremental)
+        _animatedTimeObject.tweenAnim = turbine.transform.DOLocalRotate(new Vector3(180f, 0f, 0f), speed, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Incremental)
             .SetEase(Ease.Linear).SetRelative();
+
+        _animatedTimeObject.tweenAnim.Play();
     }
 
 
--- MachSavior/Assets/_Data/Scripts/TimeSystem/Props/TurbineTrigger.cs
+++ MachSavior/Assets/_Data/Scripts/TimeSystem/Props/TurbineTrigger.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TurbineTrigger : MonoBehaviour
 {
-    [SerializeField] private AnimatedTimeObject _animatedTimeObject;
+    //[SerializeField] private AnimatedTimeObject _animatedTimeObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +18,32 @@ public class TurbineTrigger : MonoBehaviour
         
     }
 
+    private void OnDisable()
+    {
+        PlayerManager.Instance.SetIsGravityAdditionalTurbineSpeedApplied(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+        }
+    }
+
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player") && _animatedTimeObject.IsActive())
+        if (other.CompareTag("Player"))
         {
+            //PlayerManager.Instance.CalculateAdditionalTurbineSpeed(transform.up, 10f);
+            //PlayerManager.Instance.SetIsGravityAdditionalTurbineSpeedApplied(true);
+            other.GetComponent<PlayerMovement>().ResetVerticalSpeed();
+            other.GetComponent<Rigidbody>().AddForce(transform.up * 20f, ForceMode.VelocityChange);
             print("Apply air force");
+        }
+        else
+        {
+            PlayerManager.Instance.SetIsGravityAdditionalTurbineSpeedApplied(false);
         }
 
     }
